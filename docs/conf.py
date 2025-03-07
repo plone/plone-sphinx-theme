c72277305f944bb0bcbdf5f8c8a1eec67a9015c1
Steve Piercy <web@stevepiercy.com>
2025-03-07 01:02:34 -0800

    Move comments to their correct section

--- docs/conf.py
+++ docs/conf.py
@@ -395,14 +395,15 @@ redirects = {
 }
 
 
-# -- Options for HTML help output -------------------------------------------------
-
 # -- sphinx-tippy configuration ----------------------------------
 tippy_props = {
     "placement": "auto",
     "interactive": True,
 }
 tippy_anchor_parent_selector = "article.bd-article"
+
+
+# -- Options for HTML help output -------------------------------------------------
 
 # Output file base name for HTML help builder.
 htmlhelp_basename = "PloneDocumentation"
