e360dcb2418aed53a3bce40a7d6edb06b142ae2a
Steve Piercy <web@stevepiercy.com>
2025-06-30 03:02:41 -0700

    Insert heading for bz2

--- scripts/kitchen_sink_update.py
+++ scripts/kitchen_sink_update.py
@@ -28,7 +28,7 @@ for ifile in kitchen_sink_files:
     # The sphinx-themes docs expect Furo to be installed, so we overwrite w/ this path
     text = text.replace("src/furo", "src/plone_sphinx_theme")
     # Remove after https://github.com/sphinx-themes/sphinx-themes.org/pull/177 is merged
-    text = text.replace("urllib.parse", "bz2")
+    text = text.replace(".. automodule:: urllib.parse", "bz2\n***\n\n.. automodule:: bz2")
     # Add introductory message directing people to Sphinx Themes
     if "index" in ifile:
         text = text.replace("============", "============\n\n" + EXTRA_MESSAGE)
